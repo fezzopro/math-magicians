31a4d96abe7ea7a116e7d396c3a11dd7439e507e
Fezzopro <fezzopro@gmail.com>
2023-06-08 22:12:45 +0200

    Add calculator component to the application

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import calculate from '../../logic/calculate';
+import './calculator.css';
+
+const Calculator = () => {
+  const [state, setState] = useState({ next: null, operation: null, total: null });
+  const Button = ({ signs }) => {
+    const btns = signs.map((sign) => {
+      const callCalculate = (value) => {
+        const results = calculate(state, value);
+        setState((prevState) => ({ ...prevState, ...results }));
+      };
+      return (
+        <button
+          onClick={(event) => callCalculate(event.target.value)}
+          type="button"
+          className={`button button-${sign.toString()}`}
+          key={sign.toString()}
+          value={sign.toString()}
+        >
+          {sign}
+        </button>
+      );
+    });
+    return btns;
+  };
+  const displayValue = `${state.total ?? ''} ${state.operation ?? ''} ${state.next ?? ''}`;
+
+  const signs = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  return (
+    <div className="main-content">
+      <div className="calculator">
+        <input className="display-value" type="text" value={displayValue} readOnly />
+        <div className="buttons">
+          <Button signs={signs} />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Calculator;
